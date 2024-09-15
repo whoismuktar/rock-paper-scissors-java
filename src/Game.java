49250d52c8d1ce8eee4261287e21692010d42af2
whoismuktar <mayunited12@yahoo.com>
2024-09-15 21:41:08 +0100

    add player options

---
+++ src/Game.java
@@ -0,0 +1,17 @@
+
+public class Game {
+
+    String[] playerOptions = {"rock", "paper", "scissors"};
+
+    public static String roundResult() {
+        String result = "Scisors won";
+        System.out.println(result);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        roundResult();
+    }
+}
